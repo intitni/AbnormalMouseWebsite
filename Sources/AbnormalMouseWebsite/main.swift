a7b51a8da93b34c5f8bf86e9c0cf30c9ae133f85
Shangxin Guo <int123c@gmail.com>
2022-02-26 00:28:47 +0800

    Publish version 2022.2

--- Sources/AbnormalMouseWebsite/main.swift
+++ Sources/AbnormalMouseWebsite/main.swift
@@ -56,7 +56,7 @@ extension Language {
     }
 
     var downloadLink: URL {
-        URL(string: "https://github.com/intitni/AbnormalMouseApp/releases/download/version%2F2022.1/Abnormal_Mouse.zip")!
+        URL(string: "https://github.com/intitni/AbnormalMouseApp/releases/download/version%2F2022.2/Abnormal_Mouse.zip")!
     }
 
     var githubLink: URL {
